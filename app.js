e132f8b9c23250ad2712993a583e0de822247ffc
paulribet-spec <paul.ribet@gmail.com>
2025-08-19 21:52:03 -0400

    Update app.js
    se agrego un arreglo

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [] //Arreglo vacio para almacenar a los amigos que agregará el usuario de mi app
